c17bac0a5984d5e2b1998392529d8d5942c7882f
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-02-25 15:30:11 +0400

    Introducing Update tracker + new information
    now you can see how many days ago was the last update.

--- RecipeManager/Tools/TextBox.java
+++ RecipeManager/Tools/TextBox.java
@@ -1,22 +1,40 @@
 package Tools;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextPane;
+
 import main.App;
 
 public class TextBox {
+	
 	private JTextPane textPane;
-
+    private int lastDate = 25;
+    private int currentDate = new Date().getDate();
+    private App app;
+    
 	public TextBox(App app) {
-	    textPane = new JTextPane();
+		this.app = app;
+	}
+	public final void validate() {
+		textPane = new JTextPane();
 		textPane.setBounds(0, 0, 338, 123);
 		textPane.setEditable(false);
 		textPane.setFocusable(false);
 		textPane.setEnabled(false);
 		textPane.setText("                                     What's New:" +
-		                                                            "\n                                     - Improved Sound\n                                     - Dark & Light Mode\n                                     - Removed resource dependency \n\n  Released: 7.02.2026\n  Version: 1.1v");
+				"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026\n  Version: 1.2v");
 		textPane.setBorder(BorderFactory.createLineBorder(Color.black));
-		app.panels[4].add(textPane);
+		app.panelList.get(4).add(textPane);
+		
+		if(new Date().getDate() > 27) {
+			textPane.setText("                                     What's New:" +
+					"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026 (" + "long time ago)" + "\n  Version: 1.2v");
+		}else {
+			textPane.setText("                                     What's New:" +
+					"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026 (" + (Math.round(currentDate) - lastDate) + " days ago)" + "\n  Version: 1.2v");	
+		}
 	}
-	public final void validate() {}
 }
