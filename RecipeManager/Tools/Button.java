5dca1ee00434a6bd4863a191e10e987fcb250291
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-02-25 15:27:28 +0400

    Code Improvement + Button creating method
    Now you can see soundless or sound-producing button.

--- RecipeManager/Tools/Button.java
+++ RecipeManager/Tools/Button.java
@@ -6,11 +6,9 @@ import java.awt.event.*;
 import sound.Sound;
 
 public class Button {
-	public void createButton(JButton b, int[] bounds, boolean focusableStatus, Color backgroundColor, Color foregroundColor, JPanel panel, boolean visible) {
+	public void createButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible) {
         b.setFocusable(focusableStatus);
-        b.setBackground(backgroundColor);
-        b.setForeground(foregroundColor);
-        b.setBounds(bounds[0], bounds[1], bounds[2], bounds[3]);
+        b.setBounds(rect.getBounds());
         b.setVisible(visible);
         b.putClientProperty("JButton.buttonType", "roundRect");
         b.putClientProperty("FlatLaf.background", Color.white);
@@ -20,6 +18,15 @@ public class Button {
 			    new Sound().playSound();	
 			}        
         });
+        panel.add(b);
+	}
+	public void createSoundlessButton(JButton b, Rectangle rect, boolean focusableStatus, JPanel panel, boolean visible) {
+        b.setFocusable(focusableStatus);
+        b.setBounds(rect.getBounds());
+        b.setVisible(visible);
+        b.putClientProperty("JButton.buttonType", "roundRect");
+        b.putClientProperty("FlatLaf.background", Color.white);
+        b.putClientProperty("FlatLaf.foreground", Color.black);
         panel.add(b);
 	}
 }
