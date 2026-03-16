48ae639e62a443043f875cefab66241858f40e3f
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-03-16 20:24:27 +0400

    Added New Information
    so, users can see what's new from now in the latest jar.

--- RecipeManager/Tools/TextBox.java
+++ RecipeManager/Tools/TextBox.java
@@ -25,16 +25,16 @@ public class TextBox {
 		textPane.setFocusable(false);
 		textPane.setEnabled(false);
 		textPane.setText("                                     What's New:" +
-				"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026\n  Version: 1.2v");
+				"\n                                     - Fixed Sound Bugs\n                                     - Fixed Window Bugs\n                                     - Disabled Update Tracker System \n\n  Released: 16.03.2026\n  Version: 1.3v");
 		textPane.setBorder(BorderFactory.createLineBorder(Color.black));
 		app.panelList.get(4).add(textPane);
 		
 		if(new Date().getDate() > 27) {
 			textPane.setText("                                     What's New:" +
-					"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026 (" + "long time ago)" + "\n  Version: 1.2v");
+					"\n                                     - Fixed Sound Bugs\n                                     - Fixed Window Bugs\n                                     - Disabled Update Tracker System \n\n  Released: 16.03.2026 " + "(Disabled)" + "\n  Version: 1.3v");
 		}else {
 			textPane.setText("                                     What's New:" +
-					"\n                                     - Adjusted Components\n                                     - Fixed Coordination\n                                     - Added Update Tracker System \n\n  Released: 24.02.2026 (" + (Math.round(currentDate) - lastDate) + " days ago)" + "\n  Version: 1.2v");	
+					"\n                                     - Fixed Sound Bugs\n                                     - Fixed Window Bugs\n                                     - Disabled Update Tracker System \n\n  Released: 16.03.2026 (" + "Disabled)" + "\n  Version: 1.3v");	
 		}
 	}
 }
