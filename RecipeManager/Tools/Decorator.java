d186fbbd2e529d4be916a32cbfb8313ddc13de74
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-04-09 16:51:52 +0400

    Code Changes + Bug Fixes

--- RecipeManager/Tools/Decorator.java
+++ RecipeManager/Tools/Decorator.java
@@ -5,12 +5,11 @@ import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
-
 import main.App;
+import recipeSystem.RecipeHandler;
 import sound.Sound;
 
 public class Decorator implements java.awt.event.ActionListener {
-
     private App app;
     private boolean showingNext = false;
     private Timer imageTimer;
@@ -20,6 +19,7 @@ public class Decorator implements java.awt.event.ActionListener {
     private int addings = 0;
     private ResourceLoader rl;
     private Sound sound;
+    private RecipeHandler rh;
     
     {
     	Burgerpick = new JMenuItem("Pick (60$)");
@@ -27,58 +27,63 @@ public class Decorator implements java.awt.event.ActionListener {
     	popup1 = new JPopupMenu();
     	popup2 = new JPopupMenu();
     	sound = new Sound();
+    	rh = new RecipeHandler(app);
     }
     public Decorator(App app) {
         this.app = app;
         this.rl = new ResourceLoader();
     }
-    public void decorate() throws IOException {
-        Burgerpick.setEnabled(true);
-    	Burgerpick.setFocusable(false);
-        Burgerpick.setBackground(Color.white);
-        Burgerpick.setForeground(Color.black);
-        Burgerpick.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource()==Burgerpick) {
-					addings += 60;
-					Burgerpick.setText("Selecting more than once is disallowed, product is too expensive.");
-					app.labelList.get(12).setText("" + addings);
-                    Burgerpick.setEnabled(false);					
-				}
-			}        	
-        });
-        Drinkpick.setFocusable(false);
-        Drinkpick.setBackground(Color.white);
-        Drinkpick.setForeground(Color.black);
-        Drinkpick.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			    if(e.getSource()==Drinkpick) {
-			    	addings += 90;
-			    	app.labelList.get(12).setText("" + addings);
-			    }
-			}        	
-        });
-        popup1.add(Burgerpick);
-        popup1.setFocusable(false);
-        
-        popup2.add(Drinkpick);
-        popup2.setFocusable(false);
-        
-        app.labelList.get(2).setToolTipText("Burgers");
-        app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Burger.png")));
-        app.labelList.get(2).setBorder(BorderFactory.createLineBorder(Color.black));
-        app.labelList.get(2).setComponentPopupMenu(popup1);
-        app.labelList.get(1).setToolTipText("Salads");
-        app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/TomatoSalad.png")));
-        app.labelList.get(1).setBorder(BorderFactory.createLineBorder(Color.black));
-        app.labelList.get(1).setComponentPopupMenu(popup2);
-        app.buttonList.get(5).addActionListener(this);
+    public void decorate() {
+    	try {
+    		Burgerpick.setEnabled(true);
+    		Burgerpick.setFocusable(false);
+    		Burgerpick.setBackground(Color.white);
+    		Burgerpick.setForeground(Color.black);
+    		Burgerpick.addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e) {
+    				if(e.getSource()==Burgerpick) {
+    					addings += 60;
+    					Burgerpick.setText("Selecting more than once is disallowed, product is too expensive.");
+    					app.labelList.get(12).setText("" + addings);
+    					Burgerpick.setEnabled(false);					
+    				}
+    			}        	
+    		});
+    		Drinkpick.setFocusable(false);
+    		Drinkpick.setBackground(Color.white);
+    		Drinkpick.setForeground(Color.black);
+    		Drinkpick.addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e) {
+    				if(e.getSource()==Drinkpick) {
+    					addings += 90;
+    					app.labelList.get(12).setText("" + addings);
+    				}
+    			}        	
+    		});
+    		popup1.add(Burgerpick);
+    		popup1.setFocusable(false);
+    		
+    		popup2.add(Drinkpick);
+    		popup2.setFocusable(false);
+    		
+    		app.labelList.get(2).setToolTipText("Burgers");
+    		app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Burger.png")));
+    		app.labelList.get(2).setBorder(BorderFactory.createLineBorder(Color.black));
+    		app.labelList.get(2).setComponentPopupMenu(popup1);
+    		app.labelList.get(1).setToolTipText("Salads");
+    		app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/TomatoSalad.png")));
+    		app.labelList.get(1).setBorder(BorderFactory.createLineBorder(Color.black));
+    		app.labelList.get(1).setComponentPopupMenu(popup2);
+    		app.buttonList.get(5).addActionListener(this);
+    	}catch(IOException e) {
+           System.out.println("Failed to Decorate: " + e.getMessage());  		
+    	}
     }
     public void actionPerformed(ActionEvent e) {
     	if (e.getSource() == app.buttonList.get(5)) {
-		    Burgerpick.setText("Pick (60$)");
+    	    Burgerpick.setText("Pick (60$)");
     		Burgerpick.setEnabled(true);
-        	if (!showingNext) {
+    		if (!showingNext) {
         		try {
         			app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Steak.png")));
 				} catch (IOException ex) {
@@ -116,7 +121,6 @@ public class Decorator implements java.awt.event.ActionListener {
                             app.labelList.get(1).setToolTipText("Chicken Salad");
                             counter = 0; 
                         }
-                        sound.playSound();
                     }
                 });
                 imageTimer.start();
