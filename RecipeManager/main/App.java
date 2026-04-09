f068e1774c2ccc4fb52ece7a76c101cdd0195eb1
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-04-09 16:50:16 +0400

    Code Changes

--- RecipeManager/main/App.java
+++ RecipeManager/main/App.java
@@ -3,12 +3,12 @@ package main;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.Font;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import Tools.*;
 import recipeSystem.MoreTab;
 import recipeSystem.RecipeHandler;
-import Tools.*;
-import com.formdev.flatlaf.*;
+import startup.StartupScreen;
 
 public class App {
 	public Screen screen = new Screen();
@@ -17,63 +17,39 @@ public class App {
 	public TextBox tb = new TextBox(this);
     public Bounds b = new Bounds();
     private Panel panel = new Panel(screen);
-    private Button button = new Button();
     private Decorator dec = new Decorator(this);
     private RecipeHandler rh = new RecipeHandler(this);
     public MoreTab mt = new MoreTab(this, rh);
-    
-    public ArrayList<JLabel> labelList = new ArrayList<>();
-    public ArrayList<JButton> buttonList = new ArrayList<>();
-    public ArrayList<JPanel> panelList = new ArrayList<>();
-    public ArrayList<Font> FontList = new ArrayList<>();
-    
-	public App() { 
-		labelList.add(new JLabel("Foods & Drinks"));
-		labelList.add(new JLabel(""));
-		labelList.add(new JLabel(""));
-		labelList.add(new JLabel("Recipes"));
-		labelList.add(new JLabel(""));
-		labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel("Allergens:"));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel("Total Sum:"));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel("")); 
-        labelList.add(new JLabel(""));
-        labelList.add(new JLabel(""));
-		
-		buttonList.add(new JButton("Burgers:"));
-		buttonList.add(new JButton("Steak:"));
-		buttonList.add(new JButton("Ice-Cream:"));
-		buttonList.add(new JButton("Chicken Salad:"));
-		buttonList.add(new JButton("Tomato Salad:"));
-		buttonList.add(new JButton("Next"));
-		buttonList.add(new JButton("More"));
+    private Button button = new Button(this, mt);
+    public List<JLabel> labelList = new ArrayList<>();
+    public List<JButton> buttonList = new ArrayList<>();
+    public List<JPanel> panelList = new ArrayList<>();
+    public List<Font> FontList = new ArrayList<>();
+    private String[] labelTexts = {
+    	    "Foods & Drinks", "", "",
+    	    "Recipes", "", "", "", "", "",
+    	    "Allergens:", "",
+    	    "Total Sum:", "", "", "", "", "", "", ""};
+    public static String[] buttonTexts = {"Burgers:", "Steak:", "Ice-Cream:", "Chicken Salad:", "Tomato Salad:", "Next", "More", "D"};
+    private final static String runtimeType = "User Runtime";
+    private Font[] fonts = new Font[] {
+    		new Font("Tahoma", Font.BOLD, 2), new Font("Sitka Text", Font.BOLD, 31), new Font("Trebuchet MS", Font.BOLD, 18)
+    };
+    public App() { 
+		 for(String lt : labelTexts) {
+			labelList.add(new JLabel(lt));
+		}for(String bt : buttonTexts) {
+			buttonList.add(new JButton(bt));
+		}for(int i = 0; i <= 4; i++) {
+        	panelList.add(new JPanel());
+        }for(Font f : fonts) {
+        	FontList.add(f);
+        }
+        screen.createWindow();
 
-		panelList.add(new JPanel());
-		panelList.add(new JPanel());
-		panelList.add(new JPanel());
-		panelList.add(new JPanel());
-		panelList.add(new JPanel());
-	
-		FontList.add(new Font("Tahoma", Font.BOLD, 2));
-		FontList.add(new Font("Sitka Text", Font.BOLD, 31));
-		FontList.add(new Font("Trebuchet MS", Font.BOLD, 18));
-	
-		screen.createWindow();
-
-		panel.createPanel(panelList.get(0), new Bounds(0, 0, 338, 256).getBounds(), false);
-		panel.createPanel(panelList.get(1), new Bounds(450, 0, 338, 256).getBounds(), false); 
-		panel.createPanel(panelList.get(2), new Bounds(450, 256, 338, 189).getBounds(), false);
-		panel.createPanel(panelList.get(3), new Bounds(0, 378, 338, 67).getBounds(), false);
-		panel.createPanel(panelList.get(4), new Bounds(0, 256, 338, 122).getBounds(), false); 
-		
+		for(int i = 0; i < panelList.size(); i++) {
+	        panel.createPanel(panelList.get(i), new Bounds(Bounds.panelXPostions[i], Bounds.panelYPostions[i], 338, Bounds.panelHeightPostions[i]).getBounds(), false);
+		}
 		lb.createLabel(labelList.get(0), FontList.get(1), Color.yellow, new Bounds(48, 11, 260, 54).getBounds(), false, panelList.get(0));
 		lb.createLabel(labelList.get(1), null, null, new Bounds(187, 64, 151, 144).getBounds(), false, panelList.get(0));
 		lb.createLabel(labelList.get(2), null, null, new Bounds(10, 64, 151, 144).getBounds(), false, panelList.get(0));
@@ -86,7 +62,7 @@ public class App {
 		lb.createLabel(labelList.get(9), FontList.get(1), Color.red, new Bounds(93, 11, 210, 54).getBounds(), false, panelList.get(2));
 		lb.createLabel(labelList.get(10), null, null, new Bounds(93, 63, 145, 14).getBounds(), false, panelList.get(2));
 		lb.createLabel(labelList.get(11), FontList.get(1), new Color(255, 195, 51), new Bounds(86, 5, 171, 40).getBounds(), false, panelList.get(3));
-		lb.createLabel(labelList.get(12), FontList.get(2), new Color(255, 192, 51), new Bounds( 96, 38, 133, 14).getBounds(), false, panelList.get(3));
+		lb.createLabel(labelList.get(12), FontList.get(2), new Color(255, 192, 51), new Bounds(96, 38, 133, 14).getBounds(), false, panelList.get(3));
 		
 		button.createButton(buttonList.get(0), new Bounds(10, 93, 86, 23).getBounds(), false, panelList.get(1), true);
 		button.createButton(buttonList.get(1), new Bounds(10, 124, 86, 23).getBounds(), false, panelList.get(1), true);
@@ -104,43 +80,25 @@ public class App {
 			labelList.get(i).setBackground(Color.black);
 		    labelList.get(i).setFont(FontList.get(i-i));
 		}
-		sp.createSeperator(labelList.get(13), new Bounds(338, 26, 115, 42).getBounds(), true);
-		sp.createSeperator(labelList.get(14), new Bounds(338, 116, 115, 42).getBounds(), true);
-		sp.createSeperator(labelList.get(15), new Bounds(338, 214, 115, 42).getBounds(), true);
-		sp.createSeperator(labelList.get(16), new Bounds(338, 296, 115, 42).getBounds(), true);
-		sp.createSeperator(labelList.get(17), new Bounds(338, 378, 115, 42).getBounds(), true);
-		
-	    try {
-			dec.decorate();
-		} catch (IOException e) {
-		    System.out.println("Failed to decorate: " + e.getMessage());
+		for(int i = 0; i < Bounds.yPostions.length; i++) {
+			sp.createSeperator(labelList.get(13+i), new Bounds(338, Bounds.yPostions[i], 115, 42).getBounds(), true);
 		}
+		dec.decorate();
 	    rh.startRecipeSystem();
-	    
-	    tb.validate(); 
+	    tb.validate(); 	    
 	}
 	public static void main(String[] args) {
-	     SwitchRuntime("User Runtime");
+		javax.swing.SwingUtilities.invokeLater(() -> {
+	        switchRuntime("User Runtime");
+		});
 	}
-	private static void SwitchRuntime(String RuntimeType) {
-		  javax.swing.SwingUtilities.invokeLater(() -> {
-			if(RuntimeType.equals("User Runtime") || RuntimeType.equals("User Runtime".toLowerCase())) {
-				try {
-				    UIManager.setLookAndFeel(new FlatDarkLaf());
-					new startup.StartupScreen().timer.start();
-			     }catch(UnsupportedLookAndFeelException e) {
-			    		System.out.println("Failed: " + e.getMessage());  
-			      }
-				  }else if(RuntimeType.equals("Developer Runtime") || RuntimeType.equals("Developer Runtime".toLowerCase())) {
-				        try {
-						 UIManager.setLookAndFeel(new FlatDarkLaf());
-						 new App();
-					    }catch(UnsupportedLookAndFeelException e) {
-					    	System.out.println("Failed: " + e.getMessage());
-					   }
-				  }else {
-					  throw new RuntimeException("Invalid Runtime Type.");
-				  }
-			  });
-    	   }
-	 }
+	public static void switchRuntime(String runtimeType) {
+		if(runtimeType.equals("User Runtime") || runtimeType.equals("User Runtime".toLowerCase())) {
+				StartupScreen.setMode("Dark");
+				new startup.StartupScreen().timer.start();
+		}else if(runtimeType.equals("Developer Runtime") || runtimeType.equals("Developer Runtime".toLowerCase())) {
+				StartupScreen.setMode("Dark");
+                new App();
+	   }
+	}
+ }
