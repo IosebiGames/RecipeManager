b8800f613f13a5f129c234b41c797360d1b80544
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-04-23 18:44:39 +0400

    Code Changes

--- RecipeManager/Tools/Screen.java
+++ RecipeManager/Tools/Screen.java
@@ -2,10 +2,12 @@ package Tools;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
 import java.io.IOException;
 
 public class Screen {
 	public JFrame window;
+	private static JFrame anotherWindow;
 	
 	public Screen() {
 		window = new JFrame("RecipeManager");
@@ -27,5 +29,18 @@ public class Screen {
 	}
 	private void setIcon(ImageIcon icon) {
 		window.setIconImage(icon.getImage());
+	}
+	public static JFrame createWindow(String title, boolean resizable, boolean visible, int w, int h, int closeOperation, Image icon, WindowAdapter WindowH) {
+		anotherWindow = new JFrame(title);
+		anotherWindow.setResizable(resizable);
+		anotherWindow.getContentPane().setLayout(null);
+		anotherWindow.setDefaultCloseOperation(closeOperation);
+		anotherWindow.setPreferredSize(new Dimension(w, h));
+		anotherWindow.pack();
+		anotherWindow.setLocationRelativeTo(null);
+		anotherWindow.setVisible(visible);
+	    anotherWindow.setIconImage(icon);
+	    anotherWindow.addWindowListener(WindowH);
+	    return anotherWindow;
 	}
 }
