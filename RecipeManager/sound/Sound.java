7095fa2c8d706a578a274188af956dbabd79a6cb
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-05-02 12:12:16 +0400

    Code Changes

--- RecipeManager/sound/Sound.java
+++ RecipeManager/sound/Sound.java
@@ -5,22 +5,14 @@ import Tools.ResourceLoader;
 
 public class Sound {
 	private Clip clip;
-	private ResourceLoader rl = new ResourceLoader();
-    
-    public void playSound(String path, boolean preciseDebugging) { 
-    	try {
-    		clip = AudioSystem.getClip();
-    	    clip.open(AudioSystem.getAudioInputStream(getClass().getResource(rl.getSoundFile(path))));	
-    	    clip.start();
-    	    clip.addLineListener(e -> {
-    	      if(e.getType() == LineEvent.Type.STOP) { 
-    	            clip.close();
-    	      }});
-    	}catch(Exception e) {
-    		if(preciseDebugging) e.printStackTrace();	
-    		else {
-    			IO.println("Failed to play! " + e.getMessage());    	
-    		}
-    	}
-    }
+	
+    public void playSound(String path) throws Exception {
+    	clip = AudioSystem.getClip();
+    	clip.open(AudioSystem.getAudioInputStream(getClass().getResource(ResourceLoader.getSoundFile(path))));	
+    	clip.start();
+        clip.addLineListener(e -> {
+    	if(e.getType() == LineEvent.Type.STOP) { 
+    	      clip.close();
+        }});
+     }
 }
