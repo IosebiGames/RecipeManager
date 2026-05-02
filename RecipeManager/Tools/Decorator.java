d4785dfd430571ebfa68b598f40428d8277267e3
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-05-02 12:50:07 +0400

    Bug Fix
    Picking Burger action used to work with Steak too.
    User could've picked Burger once, then moved into next section and choose Steak only once too with the same logic.
    
    Now that's fixed.

--- RecipeManager/Tools/Decorator.java
+++ RecipeManager/Tools/Decorator.java
@@ -4,10 +4,8 @@ import javax.swing.*;
 import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
 import main.App;
 import recipeSystem.RecipeHandler;
-import sound.Sound;
 
 public class Decorator implements java.awt.event.ActionListener {
     private App app;
@@ -18,39 +16,44 @@ public class Decorator implements java.awt.event.ActionListener {
     private JPopupMenu popup1, popup2;
     private int addings = 0;
     private ResourceLoader rl;
-    private Sound sound;
     private RecipeHandler rh;
+    private ActionListener burgerAL, burgerAL2;
     
-    {
-    	Burgerpick = new JMenuItem("Pick (60$)");
-    	Drinkpick = new JMenuItem("Pick (90$)");
-    	popup1 = new JPopupMenu();
-    	popup2 = new JPopupMenu();
-    }
     public Decorator(App app) {
         this.app = app;
         this.rl = new ResourceLoader();
-        this.sound = new Sound();
         this.rh = new RecipeHandler(app);
+    	this.Burgerpick = new JMenuItem("Pick (60$)");
+    	this.Drinkpick = new JMenuItem("Pick (90$)");
+    	this.popup1 = new JPopupMenu();
+    	this.popup2 = new JPopupMenu();
     }
     public void decorate() {
-    	try {
+    		app.buttonList.get(5).setEnabled(false);
+        	 this.burgerAL = e -> {
+ 			   if(e.getSource()==Burgerpick) {
+ 				   addings += 60;
+ 				   app.labelList.get(12).setText("" + addings);
+ 				   Burgerpick.setText("Selecting more than once is disallowed, product is too expensive.");
+ 				   Burgerpick.setEnabled(false);
+ 				   app.buttonList.get(5).setEnabled(true);
+ 				   app.buttonList.get(5).addActionListener(this);
+ 			   }
+            };
+             this.burgerAL2 = e -> {
+ 		       if(e.getSource()==Burgerpick) {
+ 				   addings += 60;
+ 				   app.labelList.get(12).setText("" + addings);
+ 		      }
+            };
     		Burgerpick.setEnabled(true);
     		Burgerpick.setFocusable(false);
-    		Burgerpick.setForeground(Color.white);
+    		Burgerpick.setForeground(Color.black);
     		Burgerpick.setFont(new Font("Arial", Font.BOLD, 15));
-    		Burgerpick.addActionListener(new ActionListener() {
-    			public void actionPerformed(ActionEvent e) {
-    				if(e.getSource()==Burgerpick) {
-    					addings += 60;
-    					Burgerpick.setText("Selecting more than once is disallowed, product is too expensive.");
-    					app.labelList.get(12).setText("" + addings);
-    					Burgerpick.setEnabled(false);					
-    				}
-    			}        	
-    		});
+    		Burgerpick.addActionListener(burgerAL);
+    		
     		Drinkpick.setFocusable(false);
-    		Drinkpick.setForeground(Color.white);
+    		Drinkpick.setForeground(Color.black);
      		Drinkpick.setFont(new Font("Arial", Font.BOLD, 15));
     		Drinkpick.addActionListener(new ActionListener() {
     			public void actionPerformed(ActionEvent e) {
@@ -75,26 +78,17 @@ public class Decorator implements java.awt.event.ActionListener {
     		app.labelList.get(1).setBorder(BorderFactory.createLineBorder(Color.black));
     		app.labelList.get(1).setComponentPopupMenu(popup2);
     		app.labelList.get(10).setFont(new Font("Arial", Font.BOLD, 15));
-    		app.buttonList.get(5).addActionListener(this);
-    	}catch(IOException e) {
-           System.out.println("Failed to Decorate: " + e.getMessage());  		
-    	}
     }
     public void actionPerformed(ActionEvent e) {
-    	if (e.getSource() == app.buttonList.get(5)) {
-    	    Burgerpick.setText("Pick (60$)");
+    		Burgerpick.setText("Pick (60$)");
     		Burgerpick.setEnabled(true);
-    		if (!showingNext) {
-        		try {
-        			app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Steak.png")));
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-                try {
-                	app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/ChickenSalad.png")));
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
+    		Burgerpick.removeActionListener(burgerAL);
+    		Burgerpick.addActionListener(burgerAL2);
+
+    		if(!showingNext) {
+        		app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Steak.png")));
+			    app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/ChickenSalad.png")));
+				
                 app.labelList.get(2).setToolTipText("Steak");
                 app.labelList.get(1).setToolTipText("Chicken Salad");
                 app.buttonList.get(5).setText("Back");
@@ -105,38 +99,26 @@ public class Decorator implements java.awt.event.ActionListener {
                 imageTimer = new Timer(1000, new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                         counter++;
-                        if (counter == 5) {
-                            try {
-                            	app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/IceCream.png")));
-							} catch (IOException ex) {
-								ex.printStackTrace();
-							}
-                            app.labelList.get(1).setToolTipText("Ice Cream");
+                        if(counter == 5) {
+                            app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/IceCream.png")));
+						    app.labelList.get(1).setToolTipText("Ice Cream");
                         }
-                        if (counter == 12) {
-                            try {
-                            	app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/ChickenSalad.png")));
-							} catch (IOException ex) {
-								ex.printStackTrace();
-							}
-                            app.labelList.get(1).setToolTipText("Chicken Salad");
+                        if(counter == 12) {
+                            app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/ChickenSalad.png")));
+							app.labelList.get(1).setToolTipText("Chicken Salad");
                             counter = 0; 
                         }
                     }
                 });
-                imageTimer.start();
+              imageTimer.start();
             } else {
-            	Burgerpick.setEnabled(true);
-                try {
-                	app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Burger.png")));
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-                try {
-                	app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/TomatoSalad.png")));
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
+            	Burgerpick.addActionListener(burgerAL);
+            	Burgerpick.setText("Selecting more than once is disallowed, product is too expensive.");
+            	Burgerpick.setEnabled(false);
+            	
+                app.labelList.get(2).setIcon(new ImageIcon(rl.getImage("/images/Burger.png")));
+			    app.labelList.get(1).setIcon(new ImageIcon(rl.getImage("/images/TomatoSalad.png")));
+				
                 app.labelList.get(2).setToolTipText("Burgers");
                 app.labelList.get(1).setToolTipText("Salads");
                 app.buttonList.get(5).setText("Next");
@@ -147,6 +129,5 @@ public class Decorator implements java.awt.event.ActionListener {
                     counter = 0;
                 }
             }
-        }
-    }
+      }
 }
