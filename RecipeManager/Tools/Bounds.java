51024f68152c8707b2311cdf028ea2efb82e2d40
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-05-06 19:14:27 +0400

    Code Changes
    I forgot to release Changed Bounds class.

--- RecipeManager/Tools/Bounds.java
+++ RecipeManager/Tools/Bounds.java
@@ -4,8 +4,15 @@ import java.awt.Rectangle;
 
 public class Bounds {
 	private Rectangle rect;
-	
-	public Bounds() {}
+	public static int[] yPostions = new int[] 
+        	{26, 116, 214, 296, 378};
+	public static int[] panelXPostions = new int[]
+        	{0, 450, 450, 0, 0};
+	public static int[] panelYPostions = new int[] 
+        	{0, 0, 256, 378, 256};
+	public static int[] panelHeightPostions = new int[] 
+        	{256, 256, 189, 67, 122};
+    public Bounds() {}
 	public Bounds(int x, int y, int w, int h) {
 		rect = new Rectangle(x, y, w, h);
 	}
