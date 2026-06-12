c0237efb3399a7d1aa64bd58e2f3b3b0082317ac
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-06-12 10:53:47 +0400

    New Inter Font even in "More Info"

--- RecipeManager/Tools/TextBox.java
+++ RecipeManager/Tools/TextBox.java
@@ -1,79 +1,159 @@
-package Tools;
+package recipeSystem;
 
 import javax.swing.*;
 import java.awt.*;
-import java.time.*;
-import startup.StartupScreen;
+import java.awt.event.*;
+import java.util.ArrayList;
 import main.App;
+import Tools.*;
+import Tools.Button;
+import Tools.Labels;
+import Tools.Panel;
+import sound.Sound;
+import startup.StartupScreen;
 
-public class TextBox {
-	private JTextPane textPane;
+public class MoreTab {
+	private JFrame window;
     private App app;
-    private int lastDate;
-    private int latestDate;
-    private int lastMonth;
-    private int latestMonth;
-    private int daysPerMonth;
-    private String daysAgo, monthsAgo;
-    private LocalDate date;
-    private Fonts f;
+    private Panel p;
+    private RecipeHandler rh;
+    private Labels l;
+	private Button b;
+	private Fonts f;
+	private Timer resetTimer;
+    private int counter = 0;
+    private String[] text, labelTexts;
+    public ArrayList<JLabel> labelList = new ArrayList<>();
+    public ArrayList<JButton> buttonList = new ArrayList<>();
+    public ArrayList<JPanel> PanelList = new ArrayList<>();
+    private final String[] products = new String[] {"Meat", "Tomato Salad", "Chicken Salad", "Ice-Cream", "Burger"};
+    private JComboBox<?> productBox = new JComboBox<>(products);
     
-    public TextBox(App app) {
+   public MoreTab(App app, RecipeHandler rh) {
 		this.app = app;
+		this.p = new Panel(app.screen);
+		this.rh = rh;
+		this.l = new Labels(app.screen);
+		this.b = new Button();
 		this.f = new Fonts();
-		this.date = LocalDate.now();
-	    this.lastDate = 12;
-	    this.lastMonth = 6;
-	    this.latestDate = date.getDayOfMonth();
-	    this.latestMonth = date.getMonthValue();
-	    this.daysPerMonth = 31;
-	}
-	public void validate() {
-		textPane = new JTextPane();
-		textPane.setBounds(0, 0, 338, 123);
-		textPane.setEditable(false);
-		textPane.setFocusable(false);
-		textPane.setBorder(BorderFactory.createLineBorder(Color.black));
-		f.AllowExternalFont("src/fonts/Inter_bold.ttf");
+	    this.labelTexts = new String[] {"Product:", "Calories:", "Vitamins:", "Fats:", "Sodium:", "Protein:", "Water:", "Allergens:"};
+    }
+	public void openMoreTab() {
+		for(int i = 0; i <= 7; i++) {
+			labelList.add(new JLabel(labelTexts[i]));	
+		}
+		buttonList.add(new JButton("Request all Recipes"));
+		buttonList.add(new JButton("Close"));
+	    PanelList.add(new JPanel());
+	    f.AllowExternalFont("src/fonts/Inter_bold.ttf");
+	    
+		resetTimer = new Timer(1000, _ -> {
+			counter++;
+			if(counter == 4) {
+				counter = 0;
+				if(App.mode.equals("Dark")) {
+					buttonList.get(1).setBackground(Color.black);
+				}else if(App.mode.equals("Light")) {
+					buttonList.get(1).setBackground(Color.white);
+				}
+				resetTimer.stop();
+			}
+		});
+		resetTimer.start();
+		
+		window = Screen.createWindow("More Info", false, true, 306, 459, JFrame.DO_NOTHING_ON_CLOSE, new ImageIcon(getClass().getResource("/images/icon.png")).getImage(), new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+			     Toolkit.getDefaultToolkit().beep();
+			     buttonList.get(1).setBackground(Color.red);
+			     resetTimer.start();
+			     return;
+			}
+		});
+		p.createPanel(PanelList.get(0), new Bounds(29, 92, 239, 311).getBounds(), false);
+		b.createButton(buttonList.get(0), new Bounds(10, 216, 170, 41).getBounds(), false, PanelList.get(0), true, Color.white, Color.black);
+		b.createButton(buttonList.get(1), new Bounds(10, 260, 170, 41).getBounds(), false, PanelList.get(0), true, Color.white, Color.black);
+		
+		for(int i = 0; i <= 1; i++) {
+			buttonList.get(i).setBackground(Color.white);
+			buttonList.get(i).setForeground(Color.black);
+		}
+		l.createLabel(labelList.get(0), createFont("Inter", Font.BOLD, 15), Color.black, new Bounds(28, -8, 181, 63).getBounds(), false, null);
+		l.createLabel(labelList.get(1), createFont("Inter", Font.BOLD, 18), Color.black, new Bounds(10, 31, 118, 51).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(2), createFont("Inter", Font.BOLD, 16), Color.black, new Bounds(10, 73, 177, 51).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(3), createFont("Inter", Font.BOLD, 18), Color.black, new Bounds(10, 136, 175, 41).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(4), createFont("Inter", Font.BOLD, 16), Color.black, new Bounds(10, 96, 165, 41).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(5), createFont("Inter", Font.BOLD, 16), Color.black, new Bounds(10, 116, 165, 41).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(6), createFont("Inter", Font.BOLD, 16), Color.black, new Bounds(10, 156, 175, 41).getBounds(), false, PanelList.get(0));
+		l.createLabel(labelList.get(7), createFont("Inter", Font.BOLD, 16), Color.black, new Bounds(10, 176, 175, 41).getBounds(), false, PanelList.get(0));
+		
+		productBox.setFocusable(false);
+		productBox.setFont(createFont("Inter", Font.BOLD, 14));
+		productBox.setBounds(new Bounds( 88, 41, 170, 41).getBounds());
+		productBox.addActionListener(e -> {
+			  switch(productBox.getSelectedIndex()) {
+			  case 0: displayInfo("Calories: 332", "Vitamin: B12, B6, Iron", "Protein: 26g", "Water: 60mg", "Sodium: 50g", "Fat: 17g (Satured 7g)", "Allergens: 1", "Product: " + "Meat"); break;
+			  case 1: displayInfo("Calories: 120", "Vitamin: A, C, K, Folate", "Protein: 3g", "Sodium: 1.5g", "Water: 150g", "Fat: 4g", "Allergens: 1", "Product: " + "Tomato Salad"); break; 
+			  case 2: displayInfo("Calories: 300", "Vitamin: C, A, K, Folate", "Protein: 30mg", "Sodium: 350mg", "Water: 160g", "Fat: 4g", "Allergens: 1", "Product: " + "Chicken Salad"); break;
+			  case 3: displayInfo("Calories: 250", "Vitamin: A, Calcium", "Protein: 6g", "Sodium: 350mg", "Water: 0.7g", "Fat: 14g", "Allergens: 1", "Product: " + "Ice-Cream"); break;
+			  case 4: displayInfo("Calories: 500", "Vitamin: C, K", "Protein: 15g", "Sodium: 400mg", "Water: 90g", "Fat: 25g", "Allergens: 2", "Product: " + "Burger"); break;
+		    }
+			if(e.getSource() == productBox) {
+				new Sound().playSound("/sound/click_sound.wav");
+			}
+		});
+		buttonList.get(0).addActionListener(e -> {
+            if(e.getSource()==buttonList.get(0)) {
+            	rh.writeRecipe("FoodRecipes.txt", "Meats: beef, salt, pepper, garlic. \nBurgers: beef patty, bun, lettuce, tomato. \nIce-Cream: milk, cream, sugar, egg yolks. \nTomato Salad: tomato, olive oil, salt, vinegar, onion. \nChicken Salad: mayonnaise, celery, lettuce. ");
+            	rh.writeAllergen("FoodAllergens.txt", "Chicken Salad: Mayonnaise - can be allergic to some people, possible risk. \nlettuce - Pretty rare allergy. \nBurgers: Tomato - can be allergic to some people, low risk but possible. \nlettuce - Pretty rare allergy. \nMeats: Meat - can be allergic to some people, low risk in Many Regons but possible. \npepper - Pretty rare allergy. \nIce-Cream Milk - very high risk for someone with lactose intolerance. \nEgg yolks - Possible but rare allergy. Tomato: can be allergic to some people, low risk but possible. \nTomato Salad: olivie oil - No allergy but risky for people with high-cholesterol or digestive issues. \nChicken Salad: Mayonnaise - can be allergic to some people, possible risk. \nlettuce - Pretty rare allergy.");
+            	buttonList.get(0).setEnabled(false);
+            }
+		});
+		buttonList.get(1).addActionListener(_ -> {
+		    	displayInfo("Calories:", "Possible Vitamins:", "Protein:", "Sodium", "Water Content:", "Fat:", "Allergens:", "Product:");
+		    	for(JButton b : app.buttonList) {
+		    		  b.setEnabled(false);
+		    		  buttonList.get(0).setEnabled(true); 
+		    		  rh.timer.restart();
+		    		  window.dispose();
+		    	}
+		});
+		for(int labelIndex = 1; labelIndex <= 7; labelIndex++) {
+			PanelList.get(labelIndex-labelIndex).add(labelList.get(1));
+		}
+		window.getContentPane().add(PanelList.get(0));
+		window.getContentPane().add(labelList.get(0));
+		window.getContentPane().add(productBox);
 		
 		if(App.mode.equals("Dark")) {
-			textPane.setForeground(Color.white);
+			StartupScreen.setMode("Dark",  null);
+			for(int i = 0; i < labelList.size(); i++) {
+                labelList.get(i).setForeground(Color.white);
+			}
+			buttonList.get(0).setForeground(Color.white);
+			buttonList.get(0).setBackground(Color.black);
+			buttonList.get(1).setForeground(Color.white);
+			buttonList.get(1).setBackground(Color.black);
+			return;
 		}else if(App.mode.equals("Light")) {
-			textPane.setForeground(Color.black);
-		}
-		app.panelList.get(4).add(textPane);
-		if(StartupScreen.tracker_permission) {
-			textPane.setContentType("text/html");	
-			textPane.setFont(new Font("Inter", Font.PLAIN, 16));
-			setTracker(lastDate, lastMonth, latestDate, latestMonth, "Feature was disabled by Developer since April of 2026, Functionality can be limited.");
-		}else {
-			textPane.setContentType("text/html");	
-			setInformation("<html><pre style='font-family:Inter; font-size:12px;'>"
-				 + "                        What's New: " + "<br>"
-				 + "                          - Imprecise Payment Bug Fix <br>"
-				 + "                          - Better Runtime                           <br>"
-				 + "   Released: 12.06.2026 " + "<font color='red'>(Tracker Disabled)</font>"
-				 + "</pre></html>");
+			StartupScreen.setMode("Light",  null);
+			for(int i = 0; i < labelList.size(); i++) {
+                labelList.get(i).setForeground(Color.black);				
+			}
+			buttonList.get(0).setForeground(Color.black);
+			buttonList.get(0).setBackground(Color.white);
+			buttonList.get(1).setForeground(Color.black);
+			buttonList.get(1).setBackground(Color.white);
+            return;
 		}
 	}
-	private void setInformation(final String info) {
-		textPane.setText(info);
+	private Font createFont(String fontName, int type, int size) {
+		return new Font(fontName, type, size);
 	}
-	private void setTracker(int lastDate, int lastMonth, int latestDate, int latestMonth, final String warningTip) {
-		 textPane.setToolTipText(warningTip);
-		 if(latestMonth > lastMonth && latestDate == lastDate) {
-			 daysAgo = " (" + String.valueOf(latestMonth - lastMonth) + "mo)";
-			 setInformation("                        What's New: " + "\n                          - Background Bug Fixes \n                          \n   Released: 12.06.2026" + daysAgo);
-		 }else if(latestDate < lastDate) {
-			 monthsAgo = " (" + (daysPerMonth - (lastDate -= latestDate)) + "d)";
-			 setInformation("                        What's New: " + "\n                          - Background Bug Fixes \n                          \n   Released: 12.06.2026" + monthsAgo);
-		 }else if(lastDate == latestDate && lastMonth == latestMonth) {
-			 daysAgo = "(<font color='green'><b>Today</b></font>)";
-			 setInformation("<html><pre style='font-family:Inter; font-size:11px;'>" + "                        What's New: <br>"  + "                          - Background Bug Fixes <br>"  + "                          <br>" + "   Released: 12.06.2026 " + daysAgo + "</pre></html>" + daysAgo);
-		 }else if(lastDate == 0 && lastMonth == 0) {
-			 textPane.setContentType("text/plain");
-			 textPane.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 15));
-			 setInformation("\n   Tracker failed to load the last Update date. 🌧️");
-		 }
-	 }  
+	private void displayInfo(String caloriesInf, String vitaminInf, String proteinInf, String sodiumInf, String waterInf, String fatInf, String allergenInf, String productInf) {
+        text = new String[] {productInf, caloriesInf, vitaminInf, proteinInf, sodiumInf, fatInf, waterInf, allergenInf};
+        for(int i = 0; i < text.length; i++) { 
+            labelList.get(i).setText(text[i]);
+       }
+    }
 }
